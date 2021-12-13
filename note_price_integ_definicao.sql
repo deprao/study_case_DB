b2a50932a7a14961581334106b451df0c0ce9be6
Pedro Wendling <66636811+deprao@users.noreply.github.com>
2021-12-12 18:26:58 -0600

    Update note_price_integ_definicao.sql

--- note_price_integ_definicao.sql
+++ note_price_integ_definicao.sql
@@ -12,8 +12,8 @@ CREATE TABLE Loja_fornecedora(
 	PRIMARY KEY(nome)
 );
 
-CREATE TABLE Esp_tecnicas(
-	Modelo_note text PRIMARY KEY,
+CREATE TABLE Notebook_gamer(
+	Modelo text PRIMARY KEY,
 	Sist_Op text,
 	Memo_RAM text,
 	Placa_video text,
@@ -24,17 +24,17 @@ CREATE TABLE Esp_tecnicas(
 			Armazena_HD, Armazena_SSD)
 );
 
-CREATE TABLE Notebook_gamer(
+CREATE TABLE NG_Ofertado(
 	Marca text,
 	Preço float,
 	Descriçao text
-)INHERITS(Esp_tecnicas);
+)INHERITS(Notebook_gamer);
 
-CREATE TABLE Hist_notebook_dados(
+CREATE TABLE NG_Coletado(
 	Preço float,
 	Loja_retirada text REFERENCES Loja_cadastrada (Nome),
 	Data_coleta date
-)INHERITS(Esp_tecnicas);
+)INHERITS(Notebook_gamer);
 
 CREATE TABLE Avaliacoes(
 	Nota float,
@@ -44,3 +44,9 @@ CREATE TABLE Avaliacoes(
 	UNIQUE(Nome_avaliador, Data_avaliaçao, Descriçao)
 );
 
+CREATE TABLE Crawlers(
+	Versao text,
+	Data_inclusao date,
+	Data_Alteracao date,
+	Loja_coleta text PRIMARY KEY,
+);
